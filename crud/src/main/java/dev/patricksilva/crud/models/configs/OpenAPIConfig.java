1fdeb099c13ea672063436a5cf9a9a2bc47d539c
SimpMahiru <cuhoaianhvlog@gmail.com>
2025-03-31 15:10:37 +0700

    1
    Signed-off-by: SimpMahiru <cuhoaianhvlog@gmail.com>

--- crud/src/main/java/dev/patricksilva/crud/models/configs/OpenAPIConfig.java
+++ crud/src/main/java/dev/patricksilva/crud/models/configs/OpenAPIConfig.java
@@ -34,7 +34,7 @@ public class OpenAPIConfig {
 
         License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
 
-        Info info = new Info().title("Endpoints Management").version("3.0")
+        Info info = new Info().title("Endpoints Management").version("3.0")git config --global user.email "your-email@example.com"
                 .description("This API exposes endpoints to manage tutorials.")
                 .termsOfService("https://www.caixa.gov.br/terms").license(mitLicense);
 
